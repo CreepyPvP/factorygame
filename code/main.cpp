726ed676d45b114926f8f5b4a1a2c7ce7b7193b5
Korbi <korbinianfischfisch@gmail.com>
2025-03-15 11:09:18 +0100

    Basic rendering

--- code/main.cpp
+++ code/main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include "rlgl.h"
+
 #include "types.h"
 #include "state.h"
 
@@ -12,13 +14,21 @@ void LoadState()
 
     state.machine[5 + 3 * state.width].type = MachineType_Conveyor; 
 
-    state.camera.target = {0, 0};
+    state.camera.target = {-1, -1};
     // state.camera.offset = {(f32) GetScreenWidth() / 2.0f, (f32) GetScreenHeight() / 2.0f};
     state.camera.zoom = 100;
+
+    state.port_count = 2;
+    state.ports[0].x = -1;
+    state.ports[0].y = 4;
+    state.ports[0].in = true;
+
+    state.ports[1].x = state.width + 1;
+    state.ports[1].y = 2;
 }
 
 Color machine_colors[] = {
-    BLUE,
+    WHITE,
     BLUE,
     GREEN,
     RED,
@@ -38,19 +48,35 @@ int main(void)
         BeginMode2D(state.camera);
         ClearBackground(RAYWHITE);
 
+        DrawGrid(50, 1);
+
         for (i32 x = 0; x < state.width; ++x)
         {
             for (i32 y = 0; y < state.height; ++y)
             {
                 MachineType type = state.machine[x + y * state.width].type;
 
-                if (type)
+                if (!type)
                 {
                     continue;
                 }
                 
                 DrawRectangle(x , y , 1, 1, machine_colors[type]);
             }
+        }
+
+        for (u32 i = 0; i < state.port_count; ++i)
+        {
+            Port *port = state.ports + i;
+
+            Color color = BLACK;
+
+            if (port->in)
+            {
+                color = YELLOW;
+            }
+
+            DrawRectangle(port->x, port->y, 1, 1, color);
         }
 
         EndMode2D();
