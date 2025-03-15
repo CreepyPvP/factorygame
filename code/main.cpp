03b0f67d695b1636249d45def4e38fab30ebab8d
Korbi <korbinianfischfisch@gmail.com>
2025-03-15 13:52:10 +0100

    Oriented conveyors correctly

--- code/main.cpp
+++ code/main.cpp
@@ -11,9 +11,19 @@ void LoadState()
 {
     state = {};
     state.width = 10;
-    state.height = 5;
+    state.height = 10;
 
     state.machine[5 + 3 * state.width].type = MachineType_Conveyor; 
+    state.machine[5 + 3 * state.width].facing = Direction_East; 
+
+    state.machine[5 + 4 * state.width].type = MachineType_Conveyor; 
+    state.machine[5 + 4 * state.width].facing = Direction_North; 
+
+    state.machine[5 + 5 * state.width].type = MachineType_Conveyor; 
+    state.machine[5 + 5 * state.width].facing = Direction_West; 
+
+    state.machine[5 + 6 * state.width].type = MachineType_Conveyor; 
+    state.machine[5 + 6 * state.width].facing = Direction_South; 
 
     state.camera.target = {-1, -1};
     // state.camera.offset = {(f32) GetScreenWidth() / 2.0f, (f32) GetScreenHeight() / 2.0f};
@@ -35,11 +45,29 @@ Color machine_colors[] = {
     RED,
 };
 
+f32 facing_rotations[] = {
+    0,      // Direction_North,
+    270,        // Direction_East,
+    180,        // Direction_South,
+    90,        // Direction_West,
+};
+
+Vector2 facing_offsets[] = {
+    v2(0, 0),
+    v2(0, 1),
+    v2(1, 1),
+    v2(1, 0),
+};
+
+Texture2D conveyor_straight;
+
 int main(void)
 {
     InitWindow(1600, 900, "Factory game");
 
     SetTargetFPS(60);
+
+    conveyor_straight = LoadTexture("assets/machines/conveyor_straight.png");
 
     LoadState();
 
@@ -59,14 +87,15 @@ int main(void)
         {
             for (i32 y = 0; y < state.height; ++y)
             {
-                MachineType type = state.machine[x + y * state.width].type;
+                Machine *machine = state.machine + (x + y * state.width);
 
-                if (!type)
+                if (!machine->type)
                 {
                     continue;
                 }
-                
-                DrawRectangle(x , y , 1, 1, machine_colors[type]);
+
+                Vector2 pos = v2(x + facing_offsets[machine->facing].x, y + facing_offsets[machine->facing].y);
+                DrawTexturePro(conveyor_straight, {0, 0, 128, 128}, {pos.x, pos.y, 1, 1}, {}, facing_rotations[machine->facing], WHITE);
             }
         }
 
